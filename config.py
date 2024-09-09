ddb035ce427a5a75cce38f6559076965e9d158ce
mohameddeeqmohamud <mohammedmhaji04@gmail.com>
2024-09-09 03:22:50 +0300

    Mohaanbot
    Mohaanbot

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "29849573
+"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "33fe6934ad1872ce3eee24079bbbbace" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "7509519225:AAFni78bLSiM-wiIo484UjMYWSa8fK6_nLo" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.7509519225:AAFni78bLSiM-wiIo484UjMYWSa8fK6_nLo
